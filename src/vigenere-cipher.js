72bd8f0ccc29a95f5e2b435bb57a06e2a996278c
Boffin-ux <fnamily@mail.ru>
2022-04-28 19:23:49 +0300

    feat: completed Vigenere cipher example

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  directMachine = true;
+  constructor(isDirect) {
+    if (arguments.length === 0) {
+      this.directMachine = true;
+    } else {
+      this.directMachine = isDirect;
+    }
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    this.squareVigenere = [];
+    this.key = '';
+    this.string = '';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  tableVigenere() {
+    for (var i = 0; i < this.alphabet.length; i++) {
+      this.squareVigenere[i] = [...this.alphabet.slice(i), ...this.alphabet.slice(0, i)];
+    }
+  }
+  encrypt(string, key) {
+    if (!string || !key) {
+      throw new Error('Incorrect arguments!');
+    } else {
+      this.tableVigenere();
+      this.key = key.toUpperCase();
+      this.string = string.toUpperCase();
+      let result = "";
+      let countKey = 0;
+      for (let i = 0; i < this.string.length; i++) {
+        if (this.alphabet.indexOf(this.string[i]) !== -1 && this.key[countKey]) {
+          result += this.squareVigenere[this.alphabet.indexOf(this.string[i])][this.alphabet.indexOf(this.key[countKey])];
+          countKey += 1;
+        } else {
+          result += this.string[i];
+        }
+        if (countKey >= this.key.length) {
+          countKey = 0;
+        }
+      }
+      if (!this.directMachine) {
+        return result.split('').reverse().join('');
+      } else {
+        return result;
+      }
+    }
+  }
+  decrypt(string, key) {
+    if (!string || !key) {
+      throw new Error('Incorrect arguments!');
+    } else {
+      this.tableVigenere();
+      this.key = key.toUpperCase();
+      this.string = string.toUpperCase();
+      let result = "";
+      let countKey = 0;
+      for (let i = 0; i < this.string.length; i++) {
+        let interim = this.squareVigenere[this.alphabet.indexOf(this.key[countKey])].indexOf(this.string[i]);
+        if (interim !== -1 && this.key[countKey]) {
+          let row = this.alphabet.indexOf(this.key[countKey]);
+          let column = this.squareVigenere[row].indexOf(this.string[i]);
+          result += this.alphabet[column];
+          countKey += 1;
+        } else {
+          result += this.string[i];
+        }
+        if (countKey >= this.key.length) {
+          countKey = 0;
+        }
+      }
+      if (!this.directMachine) {
+        return result.split('').reverse().join('');
+      } else {
+        return result;
+      }
+    }
   }
 }
 
